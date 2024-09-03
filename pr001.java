0315b8ffeefda03e61f7e956b9801ddc372a50e8
sergeyTabakaev <tabakaevca2003@gmail.com>
2024-09-03 14:11:40 +0700

    Добавленны переменные и примеры деления

--- pr001.java
+++ pr001.java
@@ -1,5 +1,13 @@
 class pr001 {
 	public static void main(String args[]) {
-		System.out.println("Первая программа на Java");
+		System.out.println("Первая программа на Java");//Выводим строку на консоль
+		int var1 = 1024;
+		int var2;
+		//System.out.println(var2);: Ошибка: переменной не присовенно значение 
+		var2 = 411;
+		System.out.println(var1/2);
+		System.out.println("Результат целочислительного действия: " + var2/2);
+		System.out.println("Результат деления привидением типа данных: " +(double)var2/2);
+		double var3 = (double)var2/2;
 	}
 }
